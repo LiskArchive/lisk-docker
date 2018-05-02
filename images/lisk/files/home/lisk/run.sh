c8b49ff761fd8980d08808c299ebc9e158bbbd8c
Hendrik Hofstadt <me@slamper.me>
2018-05-02 19:44:19 +0200

    implement config using confd

--- images/lisk/files/home/lisk/run.sh
+++ images/lisk/files/home/lisk/run.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-function jq_config {
-	jq -c "$1" config.json |sponge config.json
-}
+# generate config
+confd -onetime -backend env
 
-jq_config ".api.access.public = true"
-
-jq_config ".consoleLogLevel = \"${LISK_CONFIG_CONSOLE_LOG_LEVEL:=info}\""
-
-if [ "${LISK_CONFIG_FORGING_WHITELIST_IP:=127.0.0.1}" != "127.0.0.1" ]
-then
-	jq_config ".forging.access.whiteList = [\"127.0.0.1\",\"$LISK_CONFIG_FORGING_WHITELIST_IP\"]"
-fi
-jq_config ".db.host = \"${LISK_CONFIG_DB_HOST:=db}\""
-jq_config ".db.port = ${LISK_CONFIG_DB_PORT:=5432}"
-jq_config ".db.database = \"${LISK_CONFIG_DB_DATABASE:=lisk_test}\""
-jq_config ".db.user = \"${LISK_CONFIG_DB_USER:=lisk}\""
-jq_config ".db.password = \"${LISK_CONFIG_DATABASE_PASSWORD:=password}\""
-
+# launch Lisk Core
 export PATH=/home/lisk/lisk-Linux-x86_64/bin:$PATH
 export LD_LIBRARY_PATH=/home/lisk/lisk-Linux-x86_64/pgsql/lib
 node app.js
